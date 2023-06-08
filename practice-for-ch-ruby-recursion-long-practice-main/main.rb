bba99466948d2b9296c04759ead8d885c91f8bb4
Justin Aitken <76704172+Cuponk@users.noreply.github.com>
2023-06-07 17:04:24 -0700

    on merge sort

--- practice-for-ch-ruby-recursion-long-practice-main/main.rb
+++ practice-for-ch-ruby-recursion-long-practice-main/main.rb
@@ -1,3 +1,5 @@
+require 'byebug'
+
 def expo1(base, num)
     return 1 if num == 0
     base * expo1(base, num - 1)
@@ -27,5 +29,73 @@ def deep_dup(arr)
     deep
 end
 
-arr = [1, [2], [3, [4]]]
-p deep_dup(arr)
+# arr = [1, [2], [3, [4]]]
+# p deep_dup(arr)
+
+def fib(n)
+    arr = [1, 1]
+    while arr.length < n
+        arr << arr[-1] + arr[-2]
+    end
+    arr[0..n]
+end
+
+# p fib(5)
+
+def fib2(n)
+    if n <= 2 
+        return [1] if n == 1
+        return [1, 1] if n == 2
+    else
+        arr = fib2(n - 1)
+        arr << arr[-1] + arr[-2]
+    end
+    arr
+end
+# p fib2(5)
+
+def bsearch(arr, n)
+    if arr.empty?
+        return nil
+    end
+    mid = arr.length/2
+    if arr[mid] == n
+        return mid
+    elsif arr[mid] > n 
+        bsearch(arr[0...mid], n)
+    elsif arr[mid] < n
+        right = bsearch(arr[mid+1..-1], n)
+        if !right
+            nil
+        else
+            mid + 1 + right
+        end
+    end
+end
+
+def merge_sort(arr)
+    return [] if arr.length == 0
+    return arr if arr.length == 1
+    merge_sort(arr[0...arr.length])
+    merge_sort(arr[arr.length..-1])
+    
+end
+
+def merge(arr1, arr2)
+    new_arr = []
+    arr1.each.with_index do |ele, idx|
+        arr2.each.with_index do |ele2, idx2|
+            if ele > ele2
+
+            end
+        end
+    end
+end
+
+p bsearch([1, 2, 3], 1) # => 0
+p bsearch([2, 3, 4, 5], 3) # => 1
+p bsearch([2, 4, 6, 8, 10], 6) # => 2
+p bsearch([1, 3, 4, 5, 9], 5) # => 3
+p bsearch([1, 2, 3, 4, 5, 6], 6) # => 5
+p bsearch([1, 2, 3, 4, 5, 6], 0) # => nil
+p bsearch([1, 2, 3, 4, 5, 7], 6) # => nil
